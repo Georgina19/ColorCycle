c88fa80046e5c2df81e69ec0cbc53ae60412da37
Georgina19 <georgina.ortiz@insbaixcamp.cat>
2013-02-26 17:02:51 +0100

    Add java files

---
+++ ColorCycle.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ColorCycle extends JFrame implements ActionListener {
+	JButton Button;
+	
+	// Constructor for ColorCycle
+	public ColorCycle(String title) {
+		super(title);
+		
+		// Creo el botó i li poso un text
+		Button = new JButton("Click me");
+		Button.addActionListener(this);
+		
+		// Afegeixo el botó al layout
+		setLayout(new FlowLayout());
+		add(Button);
+	}
+	
+	// Metode
+	public void actionPerformed(ActionEvent ev) {	
+		// Variable que equival a: getContentPane().getBackground()
+		Color getColor = getContentPane().getBackground();
+		
+		// Miro quin color toca quan l'usuari cliqui al botó
+		if (getColor.equals(Color.red)) {
+			getContentPane().setBackground(Color.green);
+		} else if (getColor.equals(Color.green)) {
+			getContentPane().setBackground(Color.blue);
+		} else if (getColor.equals(Color.blue)) {
+			getContentPane().setBackground(Color.gray);
+		} else if (getColor.equals(Color.gray)) {
+			getContentPane().setBackground(Color.red);
+		} else {
+			getContentPane().setBackground(Color.red);
+		} repaint();
+	}
+
+	
+	// Main
+	public static void main(String[] args) {
+		ColorCycle demo = new ColorCycle("Click a Button");
+		demo.setSize(400, 150);
+		demo.setVisible(true);
+	}
+}
